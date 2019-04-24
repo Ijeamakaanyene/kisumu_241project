257c8f8a9628af7de8396dc1e481e154b279d250
Ijeamaka Anyene <Ijeamaka.Anyene@gmail.com>
2019-04-24 15:28:51 -0700

    Loading initial R Script to Github. Data cleaning for the csv file completed. - Recoded electricity at home, changed variables into factors, and reduced data frame to only columns needed.

---
+++ kisumu_data.R
@@ -0,0 +1,36 @@
+#####################
+### Packages      ###
+#####################
+
+library(dplyr)
+
+
+#####################
+### Data Cleaning ###
+#####################
+
+## Recommendations - convert data file into a csv on your computer. Then update the below code with your file path string
+kisumu_data = read.csv("/Users/ijeamakaanyene/kisumu_241project/Kisumu Street Youth Seroprevalence Data 9_28_2015.csv", header = TRUE)
+
+## Outcome: Glue Sniffing. Use variable glue.ever: 0 - no, 1 - yes
+
+## Exposure: Length of Time on Street. Use variable onstrt_new: 0 - <1yr, 1 - >=1 yr
+kisumu_data$onstrt_new = as.factor(kisumu_data$onstrt_new)
+
+## Confounders: 
+#Education. Use variable edatt_cat_new: 0 is <= grade 5, 1 is > grade 5
+kisumu_data$edatt_cat_new = as.factor(kisumu_data$edatt_cat_new)
+
+#Age. Use variable age (keeping continuous)
+
+#Electricity at Home. Use variable elec: 0 = no electricity, 1 = electricity 
+kisumu_data = kisumu_data %>%
+  mutate(elec = if_else(electricity == "yes", 1, 
+                       if_else(electricity == "no", 0, 
+                               if_else(is.na(electricity) == TRUE, 2, 2))))
+kisumu_data$elec = as.factor(kisumu_data$elec)
+
+kisumu_filtered = kisumu_data %>%
+  filter(ever.homeless == "yes") %>%
+  select(subject.identification.number, glue.ever, onstrt_new, edatt_cat_new, age, elec) %>%
+  na.omit(kisumu_data)
