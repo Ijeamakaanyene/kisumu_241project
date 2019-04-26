e6ef5c9e0ec75c4717b75356a6ae243de008d0eb
Emily Liu <emilyliu@Emilys-MacBook-Air.local>
2019-04-25 17:53:55 -0700

    Fixed for data cleaning probs (outcome of glue)

--- kisumu_data.R
+++ kisumu_data.R
@@ -3,6 +3,8 @@
 #####################
 
 library(dplyr)
+library(lmtest)
+library(ResourceSelection)
 
 
 #####################
@@ -13,6 +15,7 @@ library(dplyr)
 kisumu_data = read.csv("/Users/emilyliu/Desktop/Spring_2019/241 Stats/kisumu_241project/Kisumu Street Youth Seroprevalence Data 9_28_2015.csv", header = TRUE)
 
 ## Outcome: Glue Sniffing. Use variable glue.ever: 0 - no, 1 - yes
+
 
 ## Exposure: Length of Time on Street. Use variable onstrt_new: 0 - <1yr, 1 - >=1 yr
 
@@ -32,101 +35,96 @@ kisumu_data = kisumu_data %>%
 kisumu_filtered = kisumu_data %>%
   filter(ever.homeless == "yes") %>%
   select(subject.identification.number, glue.ever, onstrt_new, edatt_cat_new, age, elec) %>%
-  na.omit(kisumu_data)
+  mutate(glue = if_else(glue.ever == "yes", 1, if_else(glue.ever == "no", 0, 2))) %>% na.omit(kisumu_data) %>% 
+  filter(glue != 2)
 
-kisumu_filtered = kisumu_filtered %>% mutate(age_centered = age - 17)
+#kisumu_filtered = kisumu_filtered %>% mutate(age_centered = age - 17)
 
 ###############################
 ### 1st Logistic Regression ###
 ###############################
-#Length of Time (<1 yr vs. > 1 yr) and Glue Sniffing (unadjusted)
+
+# PAIRWISE COMPARISONS
+
+# Length of Time (<1 yr vs. > 1 yr) and Glue Sniffing (unadjusted)
+# p-value: 0.0718
 
 timestreet.v.glue <- glm(formula=glue.ever ~ onstrt_new,
                          family=binomial(link='logit'), data=kisumu_filtered)
 summary(timestreet.v.glue)
 
-#Our coefficient estimate is 0.02469. 
-#At an alpha of 0.05, we fail to reject the null with a p-value of 0.971
 
-#Education and Glue Sniffing
+# Education and Glue Sniffing
+# p-value: 0.00292
 
 education.v.glue <- glm(formula=glue.ever ~ edatt_cat_new,
                         family=binomial(link='logit'), data=kisumu_filtered)
 
 summary(education.v.glue)
-#Our coefficient estimate for education is 0.2742. At an alpha of 0.05, we fail to reject the 
-#null with a p-value of .615
 
 #Age and Glue Sniffing
+# p-value: 0.69
 
 age.v.glue <- glm(formula=glue.ever ~ age,
                   family=binomial(link='logit'), data=kisumu_filtered)
 summary(age.v.glue)
 
-#Our coefficient estimate for age is 0.2885. At an alpha of 0.05, we
-#reject the null with a p-value of 0.0442.
-
-#Electricity at home and glue sniffing 
+# Electricity at home and glue sniffing 
+# p-vlue: 0.0864
 
 elec.v.glue <- glm(formula=glue.ever ~ elec,
                    family=binomial(link='logit'), data=kisumu_filtered)
 summary(elec.v.glue)
 
-#Our coefficient estimate for electricity is -.9869. At an alpha of 0.05, we fail to 
-#reject the null with a p-value of 0.106.
+# CREATING MODELS
+#For each covariate with a p-value < .2 add to model
 
-#For each covariate with a p-value < .2 add to model#
+model1 <- glm(formula=glue.ever ~ onstrt_new + edatt_cat_new, 
+              family=binomial(link='logit'), data=kisumu_filtered)
+summary(model1)
 
-beautifulmodelone <- glm(formula=glue.ever ~ age + elec,
-                         family=binomial(link='logit'), data=kisumu_filtered)
-summary(beautifulmodelone)
-
-othermodelone <- glm(formula=glue.ever ~ age,
+model2 <- glm(formula=glue.ever ~ onstrt_new + elec + edatt_cat_new,
                      family=binomial(link='logit'), data=kisumu_filtered)
-summary(othermodelone)
+summary(model2)
 
-library(lmtest)
-lrtest(beautifulmodelone, othermodelone)
-##We fail to reject the null that one model is better than the other. 
-##Therefore, we can opt to use the model with age and electricity (beautifulmodelone).
+# LIKELIHOOD RATIO TEST
+lrtest(model1, model2)
 
-##Assessing multicollinearity
-summary(beautifulmodelone)$coef
+# We fail to reject the null that more complex model is better than the simpler model 
+# Therefore, we can opt to use the model with age (model 1)
 
+# ASSESSING COLLINEARITY
 vars <- kisumu_filtered %>%
-  dplyr::select(age, elec)
+  dplyr::select(onstrt_new, edatt_cat_new)
 
+cor(vars)
 
 ###############################
 ### 2nd Logistic Regression ###
 ###############################
 
-# Interaction between length on street and education
-# No significant interaction
-edu_length_time_lm = glm(glue.ever ~ onstrt_new + edatt_cat_new + edatt_cat_new * onstrt_new, family = binomial(link = "logit"), data = kisumu_filtered)
-summary(edu_length_time_lm)
-
 # Interaction between length on street and age
 # No significant interaction
-age_length_time = glm(glue.ever ~ onstrt_new + age + onstrt_new * age, family = binomial(link = "logit"), data = kisumu_filtered)
+age_length_time = glm(glue.ever ~ onstrt_new + age_centered + onstrt_new * age_centered, 
+                      family = binomial(link = "logit"), data = kisumu_filtered)
+
 summary(age_length_time)
 
 # Interaction between length on street and electricity
 # No significant interaction
-elec_length_time = glm(glue.ever ~ onstrt_new + elec + elec * onstrt_new, family = binomial(link = "logit"), data = kisumu_filtered)
+elec_length_time = glm(glue.ever ~ onstrt_new + elec + elec * onstrt_new, 
+                       family = binomial(link = "logit"), data = kisumu_filtered)
+
 summary(elec_length_time)
 
 # Interaction between age and electricity
 # Interesting interaction / p-value of 0.0536
-elec_age = glm(glue.ever ~ onstrt_new + elec + age + elec * age, family = binomial(link = "logit"), data = kisumu_filtered)
+elec_age = glm(glue.ever ~ onstrt_new + elec + age_centered + elec * age_centered, 
+               family = binomial(link = "logit"), data = kisumu_filtered)
+
 summary(elec_age)
 
-# Interaction between age and education
-# No significant interaction
-age_edu = glm(glue.ever ~ onstrt_new + age + edatt_cat_new + age * edatt_cat_new, family = binomial(link = "logit"), data = kisumu_filtered)
-summary(age_edu)
-
-# Assess collinearity
+# ASSESS COLLINEARITY
 # Electricity and the interaction term are highly correlated (0.98887291)
 vars_int = kisumu_filtered %>%
   mutate(int = elec*age_centered) %>%
@@ -134,9 +132,17 @@ vars_int = kisumu_filtered %>%
 
 cor(vars_int)
 
-# Centering age (see above)
-# Median age = 17
-summary(kisumu_filtered$age)
+# LIKELIHOOD RATIO TEST 
+# Comparing interaction model with non-interaction model
+# We obtain a p-value of 0.0407, can reject the null hypothesis that the simpler model is better. 
+# We go with the model with the interaction. 
+
+lrtest(elec_age, model1)
+
+# GOODNESS OF FIT TEST
+
+hoslem.test(kisumu_filtered$glue.ever, fitted(elec_age))
+
 
 
 
