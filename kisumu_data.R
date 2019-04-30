1e80f3fc6be3fb695297d19ac41e0d7284a64ede
Thien-An Ha <thienanha94@gmail.com>
2019-04-29 18:17:45 -0700

    Changed pairwise comparisons to include orphans etc.

--- kisumu_data.R
+++ kisumu_data.R
@@ -15,7 +15,7 @@ library(ResourceSelection)
 
 
 ## Recommendations - convert data file into a csv on your computer. Then update the below code with your file path string
-kisumu_data = read.csv("/Users/ijeamakaanyene/Desktop/Berkeley Coursework/Spring_2019/Statistical Analysis of Categorical Data/Assignments/Final Projects/Kisumu Street Youth Seroprevalence Data 9_28_2015.csv", header = TRUE)
+kisumu_data = read.csv("~/Berkeley/Spring 2019/PH 241/Kisumu Street Youth Seroprevalence Data 9_28_2015.csv", header = TRUE)
 
 ## Exposure: Education. Use variable edatt_cat_new: 0 is <= grade 5, 1 is > grade 5
 
@@ -53,26 +53,25 @@ kisumu_filtered = kisumu_data %>%
 
 # PAIRWISE COMPARISONS
 
-# Test out orphan status as a confounder
 
 # Education and Length of Time on Street (unadjusted)
-# p-value: 0.309 
+# p-value: 0.522 
 edu.v.timestreet = glm(onstrt_new ~ edatt_cat_new, family = binomial, data = kisumu_filtered)
 summary(edu.v.timestreet)
 
-# Length of Time (<1 yr vs. > 1 yr) and Glue Sniffing (unadjusted)
-# p-value: 0.0663
-timestreet.v.glue <- glm(onstrt_new ~ glue,
+# Orphan Status & Time on street (unadjusted )
+# p-value: 0.03823
+orphan.v.timestreet<- glm(onstrt_new ~ orphan,
                          family=binomial(link='logit'), data=kisumu_filtered)
-summary(timestreet.v.glue)
+summary(orphan.v.timestreet)
 
 # Age and Length on Time on Street (unadjusted)
-# p-value: 2.39e-07
+# p-value: 8.98e-07
 age.v.timestreet = glm(onstrt_new ~ age, family = binomial, data = kisumu_filtered)
 summary(age.v.timestreet)
 
 # Electricity at home and length of time on street (unadjusted)
-# p-vlue: 0.422
+# p-vlue: 0.434
 elec.v.timestreet = glm(onstrt_new ~ elec,
                    family=binomial(link='logit'), data=kisumu_filtered)
 summary(elec.v.timestreet)
@@ -80,8 +79,8 @@ summary(elec.v.timestreet)
 # CREATING MODELS
 #For each covariate with a p-value < .2 add to model
 
-# Comparing Models w/ and without Glue
-model1 <- glm(onstrt_new ~ edatt_cat_new + glue + age, 
+# Comparing Models w/ and without Orphan
+model1 <- glm(onstrt_new ~ edatt_cat_new + orphan + age, 
               family=binomial(link='logit'), data=kisumu_filtered)
 summary(model1)
 
@@ -91,27 +90,18 @@ model2 <- glm(onstrt_new ~ edatt_cat_new + age,
 summary(model2)
 
 
-#Comparing Models w/ and without Electricity 
-model3 = glm(onstrt_new ~ edatt_cat_new + age + elec, 
-             family = binomial, data = kisumu_filtered)
-summary(model3)
-
-model4 = glm(onstrt_new ~ edatt_cat_new + age, 
-             family = binomial, data = kisumu_filtered)
-summary(model4)
 
 # LIKELIHOOD RATIO TEST
 lrtest(model1, model2)
+# P-value: 0.1106
 # We fail to reject the null that the simpler is better than the complex
-# Therefore, we can opt to use the model with glue and age (model 2)
+# Therefore, we can opt to use the model with electricity (model 2)
 
-lrtest(model3, model4)
-# We fail to reject the null that the simpler is better than complex. 
-# Therefore, we can opt to use the model with electrity and age (model 4)
+
 
 # ASSESSING COLLINEARITY
 vars <- kisumu_filtered %>%
-  dplyr::select(onstrt_new, edatt_cat_new, age, glue)
+  dplyr::select(onstrt_new, edatt_cat_new, age, orphan, elec)
 
 cor(vars)
 
